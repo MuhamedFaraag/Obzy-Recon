11b57cb6f15e26ab75fe58c87a63101d8cc950a5
ElGeg <elgeg@ElGeg.ElGeg>
2026-03-08 06:25:38 +0200

    [+] Go_installation

--- modules/install_go.sh
+++ modules/install_go.sh
@@ -1,10 +1,27 @@
 #! /bin/bash
 
+#check Sudo
+
+if [ "$EUID" -ne 0 ]; then
+	echo "Please run with sudo permission"
+	exit 0
+fi
+
 # Check Version Function
 checkVersion(){
 	 curl -s https://go.dev/dl/?mode=json | jq -r '.[0].version'
 }
+checkIfInstalled(){
+if command -v go &> /dev/null; then
+	echo "Go Installed: $(go version)"
+	exit 0
+fi
 
+}
+cleanUP(){
+rm -rf "$FILE"
+exit 0
+}
 # Varabile to hold go version
 version=$(checkVersion)
 echo "Latest Go Version is $version"
@@ -15,16 +32,20 @@ echo "$FILE"
 GO_URL="https://golang.org/dl/$FILE"
 echo "$GO_URL"
 
-if command -v go &> /dev/null; then
-	echo "Go installed: $(go version)"
-	exit 0
-fi
+checkIfInstalled
 
 if [ ! -f "$FILE" ]; then
 	echo "Downloading $FILE...."
 	wget -c "$GO_URL" -O  "$FILE"
-fi 
-# go1.26.1
-# go1.26.1.linux-amd64.tar.gz
-# https://golang.org/dl/go1.26.1.linux-amd64.tar.gz
+fi
 
+echo "Extracting"
+rm -rf /usr/local/go && tar -C /usr/local -xzf "$FILE"
+
+echo "Setup Go"
+export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
+source ~/.zshrc
+
+cleanUP
+checkIfInstalled
